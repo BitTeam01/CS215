010b74257fb453b586b99b880c268c1f56c2e274
Bit Team <49376633+BitTeam01@users.noreply.github.com>
2019-04-08 01:42:48 +0200

    Add files via upload

---
+++ Lecture 2/Even or odd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+    int n;
+    cout<<"Enter any number: "<<endl;
+    cin>>n;
+    if(n % 2 == 0)
+    {
+        cout<<"the number is even";
+
+    }
+    else
+    {
+        cout<<"the number is odd";
+    }
+}
